99b388b6c63df49b1889e4db8eb1461b6d1807b6
Subramanian <selvags199@gmail.com>
2024-11-29 20:17:46 +0530

    Cookie Policy Added for Docker Support

--- src/Play.Identity.Service/Startup.cs
+++ src/Play.Identity.Service/Startup.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using GreenPipes;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -60,6 +63,7 @@ namespace Play.Identity.Service
                         options.Events.RaiseFailureEvents = true;
                         options.Events.RaiseSuccessEvents = true;
                         options.IssuerUri = identityServerSettings.IssuerURI;
+                        options.KeyManagement.KeyPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
                     })
                     .AddAspNetIdentity<ApplicationUser>()
                     .AddInMemoryApiScopes(identityServerSettings.ApiScopes)
@@ -103,6 +107,10 @@ namespace Play.Identity.Service
             app.UseAuthorization();
 
             app.UseStaticFiles();
+
+            app.UseCookiePolicy(new CookiePolicyOptions {
+                MinimumSameSitePolicy = SameSiteMode.Lax
+            });
 
             app.UseEndpoints(endpoints =>
             {
